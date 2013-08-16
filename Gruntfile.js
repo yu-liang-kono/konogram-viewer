c003caac2d632f27c071924b574802837059a102
Yu Liang <yu.liang@thekono.com>
2013-08-16 19:56:43 +0800

    create Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function (grunt)
+{
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            build: {
+                src: 'app/scripts/main.js',
+                dest: 'app/scripts/main.min.js'
+            }
+        },
+        cssmin: {
+            build: {
+                src: 'app/styles/main.css',
+                dest: 'app/styles/main.min.css'
+            }
+        },
+        shell: {
+            livescript: {
+                command: 'lsc -c app/scripts/controllers/*.ls'
+            }
+        },
+        concat: {
+            build: {
+                src: [
+                    'app/scripts/controllers/NavbarController.js',
+                    'app/scripts/controllers/SearchController.js',
+                    'app/scripts/controllers/ExhibitionController.js'
+                ],
+                dest: 'app/scripts/main.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('default', ['shell', 'concat', 'uglify', 'cssmin']);
+};
